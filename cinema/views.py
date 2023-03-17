c8fe5a2f54a276449153b99e346b981ad42ac25f
“Jake <2712041N@student.gla.ac.uk>
2023-03-17 11:26:01 +0000

    Leave Review

--- cinema/views.py
+++ cinema/views.py
@@ -103,21 +103,27 @@ def leave_review(request, film_title_slug):
     if film is None:
         return redirect('/cinema/')
     
+    user = request.user
+
     form = ReviewForm()
 
+    print(request.method)
     if request.method == 'POST':
+        print(film.IMDB_num)
         form = ReviewForm(request.POST)
 
-    if form.is_valid():
-        if film:
-            review = form.save(commit=False)
-            review.IMDB_num = film
-            review.user = "Jake"
-            review.save()
+        if form.is_valid():
+            if film:
+                review = form.save(commit=False)
+                review.IMDB_num = film
+                review.user = user
+                review.likes = 0
+                review.dislikes = 0
+                review.save()
 
-            return redirect(reverse('cinema:leave_review', kwargs={'film_title_slug':film_title_slug}))
+                return redirect(reverse('cinema:reviews', kwargs={'film_title_slug':film_title_slug}))
         else:
             print(form.errors)
     
-    context_dict = {'form': form, 'film': film}
+    context_dict = {'form': form, 'film': film, 'user': user}
     return render(request, 'cinema/leave_review.html', context=context_dict)
