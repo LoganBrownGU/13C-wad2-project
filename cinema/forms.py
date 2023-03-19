4c66196a44dfa7e1b5befba659a6343082644c64
Shalaj Wadhwa <2639429w@student.gla.ac.uk>
2023-03-19 17:24:43 +0000

    added bootstrap to user auth

--- cinema/forms.py
+++ cinema/forms.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control'}),
+                   'email': forms.TextInput(attrs={'class': 'form-control'}),
+                   'password': forms.PasswordInput(attrs={'class': 'form-control'}),}
