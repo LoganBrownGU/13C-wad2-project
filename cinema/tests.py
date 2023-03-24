724fe8bccfc23eec1954978897efcee9f9684c21
SamBradley1 <122485035+SamBradley1@users.noreply.github.com>
2023-03-24 15:42:54 +0000

    test

--- cinema/tests.py
+++ cinema/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from cinema.models import Review, Film
 
 from django.test import TestCase
 from django.urls import reverse
@@ -14,9 +15,24 @@ def add_user(username, email, password):
 
     return new_user
 
+def add_film(IMDB_num, title, release, cast, director, age_rating):
+    new_film = Film()
+    new_film.IMDB_num = IMDB_num
+    new_film.title = title
+    new_film.release = release
+    new_film.cast = cast
+    new_film.director = director
+    new_film.age_rating = age_rating
+    new_film.save()
+
+    return new_user
+
 
 def make_user_john():
     add_user("JohnDaGoat12", "johnnyb2003@gmail.com", "John12345")
+    
+def make_film():
+    add_film("tt001", "The best movie", 2000-11-11, "John", "John", "10")
 
 
 class UserTests(TestCase):
