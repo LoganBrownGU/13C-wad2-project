a0417263eaf1b984441f58ec901e7cdb14760094
Logan Brown <2641407B@student.gla.ac.uk>
2023-03-24 17:00:55 +0000

    search bar test

--- cinema/tests.py
+++ cinema/tests.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
+from cinema.models import Film
 
 from django.test import TestCase
 from django.urls import reverse
+from datetime import datetime, timedelta
+import time
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
 def add_user(username, email, password):
@@ -14,6 +20,15 @@ def add_user(username, email, password):
 
     return new_user
 
+
+def sanitise(string):
+    out = ""
+
+    for c in string:
+        if str(c).isalnum() or c == " ":
+            out += str(c)
+
+    return out
 
 def make_user_john():
     add_user("JohnDaGoat12", "johnnyb2003@gmail.com", "John12345")
@@ -28,3 +43,33 @@ class UserTests(TestCase):
     # def test_user_redirected(self):
     #     response = self.client.get(reverse("cinema:manager"))
     #     self.assertEquals(response.status_code, 302)
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    #fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_no_filter(self):
+        num = 0
+        film_names = ["the", "the a", "the b", "b the", "c the c"]
+        for fn in film_names:
+            Film.objects.get_or_create(IMDB_num=num, title=fn, release=datetime.now() - timedelta(days=365*(10-num)), cast="shdfisidf", director="ufh", age_rating="sfih")
+            num += 1
+
+            print(datetime.now() - timedelta(days=365*(10-num)))
+
+        self.selenium.get(str(self.live_server_url) + '/cinema/search/?search=the')
+        films = self.selenium.find_element(By.CSS_SELECTOR, "#films")  
+        films = films.find_elements(By.TAG_NAME, "a")
+
+        for f, fn in zip(films, film_names):
+            self.assertEqual(f.text, fn)
